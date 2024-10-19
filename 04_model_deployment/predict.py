cd45377678efaa8153c91ee5e842c4263bf017a9
Naga Manohar Yelubandi <61689114+Naga-Manohar-Y@users.noreply.github.com>
2024-10-19 22:15:01 +0000

    accessing model as web_service.

--- 04_model_deployment/predict.py
+++ 04_model_deployment/predict.py
@@ -1,36 +1,32 @@
 import pickle
 
-input_file = 'model_C=1.0.bin'
+from flask import Flask
+from flask import request
+from flask import jsonify
 
-with open(input_file, 'rb') as f_in: 
+
+model_file = 'model_C=1.0.bin'
+
+with open(model_file, 'rb') as f_in:
     dv, model = pickle.load(f_in)
 
+app = Flask('churn')
 
-customer = {
-    'gender': 'female',
-    'seniorcitizen': 0,
-    'partner': 'yes',
-    'dependents': 'no',
-    'phoneservice': 'no',
-    'multiplelines': 'no_phone_service',
-    'internetservice': 'dsl',
-    'onlinesecurity': 'no',
-    'onlinebackup': 'yes',
-    'deviceprotection': 'no',
-    'techsupport': 'no',
-    'streamingtv': 'no',
-    'streamingmovies': 'no',
-    'contract': 'month-to-month',
-    'paperlessbilling': 'yes',
-    'paymentmethod': 'electronic_check',
-    'tenure': 1,
-    'monthlycharges': 29.85,
-    'totalcharges': 29.85
-}
+@app.route('/predict', methods=['POST'])
+def predict():
+    customer = request.get_json()
 
-X = dv.transform([customer])
+    X = dv.transform([customer])
+    y_pred = model.predict_proba(X)[0, 1]
+    churn = y_pred >= 0.5
 
-y_pred = model.predict_proba(X)[0, 1]
+    result = {
+        'churn_probability': float(y_pred),
+        'churn': bool(churn)
+    }
 
-print('input:', customer)
-print('output:', y_pred)
+    return jsonify(result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9696)
